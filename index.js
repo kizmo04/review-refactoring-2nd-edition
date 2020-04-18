cc94b3b956d14707d77ea89c74825c25e4cadd0b
kizmo04 <get.kizmo@gmail.com>
2020-04-18 19:17:27 +0900

    :recycle: ch1.4 - 적립 포인트 계산 코드 추출하기

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    // 포인트를 적립한다
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
@@ -26,6 +22,14 @@ function statement(invoice, plays) {
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
   return result;
 }
 
