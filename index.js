3b1a2885e82a19d34d1f1632e46912b57b84a057
kizmo04 <get.kizmo@gmail.com>
2020-04-18 18:53:00 +0900

    :recycle: ch1.4 - play변수 제거하기 - playFor

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // 포인트를 적립한다
@@ -31,27 +31,31 @@ function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(perf, play) {
-  let thisAmount = 0;
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
+function amountFor(aPerformance, play) {
+  let result = 0;
 
   switch (play.type) {
     case "tragedy": // 비극
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy": // 희극
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
 
 console.log(statement(invoice, plays));
