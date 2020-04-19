98d11af0417a6d81689ac939fb713513adb610ec
kizmo04 <get.kizmo@gmail.com>
2020-04-19 16:36:12 +0900

    :recycle: 1.6 계산 단계와 포맷팅 단계 분리하기 - 함수 추출하기

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const invoice = require("./invoices.json")[0];
 const plays = require("./plays.json");
 
 function statement(invoice, plays) {
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  return renderPlainText(invoice, plays);
+}
 
+function renderPlainText(invoice, plays) {
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
@@ -37,41 +40,41 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
   }
-}
 
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === playFor(aPerformance).type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-
-  switch (playFor(aPerformance).type) {
-    case "tragedy": // 비극
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy": // 희극
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
   }
-  return result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
 }
 
 module.exports = {
