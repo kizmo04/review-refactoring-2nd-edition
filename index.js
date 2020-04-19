b5d5e6532712a918aaf0444cad94be256fb61b65
kizmo04 <get.kizmo@gmail.com>
2020-04-19 15:50:34 +0900

    :bug: 생략된 )를 추가

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const plays = require("./plays.json");
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `청구 내역 (고객명: ${invoice.customer}\n`;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
@@ -63,4 +63,6 @@ function amountFor(aPerformance) {
   return result;
 }
 
-console.log(statement(invoice, plays));
+module.exports = {
+  statement,
+};
