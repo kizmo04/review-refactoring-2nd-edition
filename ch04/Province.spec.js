dd2b52dd1f91590a1f8e0224fd1b91bd77935c4b
kizmo04 <get.kizmo@gmail.com>
2020-05-31 23:11:10 +0900

    :white_check_mark: 4.6 경계 조건 검사하기

--- ch04/Province.spec.js
+++ ch04/Province.spec.js
@@ -2,7 +2,7 @@ const Province = require("./Province");
 const sampleProvinceData = require("./sampleProvinceData");
 
 describe("Province", () => {
-  let asia;
+  let noProducers;
   beforeEach(() => {
     // given
     asia = new Province(sampleProvinceData());
@@ -23,5 +23,66 @@ describe("Province", () => {
     // then
     expect(asia.shortfall).toEqual(-6);
     expect(asia.profit).toEqual(292);
+  });
+
+  it("zero demand", () => {
+    // when
+    asia.demand = 0;
+
+    // then
+    expect(asia.shortfall).toEqual(-25);
+    expect(asia.profit).toEqual(0);
+  });
+
+  it("negative demand", () => {
+    // when
+    asia.demand = -1;
+
+    // then
+    expect(asia.shortfall).toEqual(-26);
+    expect(asia.profit).toEqual(-10);
+  });
+
+  it("empty string demand", () => {
+    // when
+    asia.demand = "";
+
+    // then
+    expect(asia.shortfall).toEqual(NaN);
+    expect(asia.profit).toEqual(NaN);
+  });
+});
+
+describe("no producers", () => {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+
+  it("shortfall", () => {
+    expect(noProducers.shortfall).toEqual(30);
+  });
+
+  it("profit", () => {
+    expect(noProducers.profit).toEqual(0);
+  });
+});
+
+describe("string for producers", () => {
+  it("", () => {
+    const data = {
+      name: "String producers",
+      producers: "",
+      demand: 30,
+      price: 20,
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).toEqual(0);
   });
 });
