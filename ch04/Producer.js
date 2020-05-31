2238efe3c870ed8bf7f589e6ff4efce7f3465589
kizmo04 <get.kizmo@gmail.com>
2020-05-31 20:19:07 +0900

    :sparkles: 4장 예시 작성

---
+++ ch04/Producer.js
@@ -0,0 +1,32 @@
+class Producer {
+  constructor(aProvince, data) {
+    const { cost, name, production } = data;
+
+    this._province = aProvince;
+    this._cost = cost;
+    this._name = name;
+    this._production = production || 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get const() {
+    return this._cost;
+  }
+  set cost(arg) {
+    this._cost = parseInt(arg);
+  }
+
+  get production() {
+    return this._production;
+  }
+  set production(amountStr) {
+    const amount = parseInt(amountStr);
+    const newProduction = Number.isNaN(amount) ? 0 : amount;
+
+    this._province.totalProduction += newProduction - this._province;
+    this._production = newProduction;
+  }
+}
