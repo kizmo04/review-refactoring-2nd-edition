3ba2771f1df607e950634729d497af4c9e1501fa
kizmo04 <get.kizmo@gmail.com>
2020-07-05 17:19:27 +0900

    test: 10장 테스트 작성

---
+++ ch10/index.spec.ts
@@ -0,0 +1,31 @@
+import rating from './index';
+
+describe('변형 동작을 다형성으로 표현하기', () => {
+  describe('Rating', () => {
+    const voyage = { zone: '서인도', length: 10 };
+    const history = [
+      { zone: '동인도', profit: 5 },
+      { zone: '서인도', profit: 15 },
+      { zone: '중국', profit: -2 },
+      { zone: '서아프리카', profit: 7 },
+    ];
+
+    it('', () => {
+      const myRating = rating(voyage, history);
+
+      const {
+        value,
+        voyageProfitFactor,
+        voyageRisk,
+        captainHistoryRisk,
+        hasChinaHistory,
+      } = myRating;
+
+      expect(value).toBe('B');
+      expect(voyageRisk).toBe(5);
+      expect(voyageProfitFactor).toBe(2);
+      expect(captainHistoryRisk).toBe(6);
+      expect(hasChinaHistory).toBe(true);
+    });
+  });
+});
--- ch10/index.ts
+++ ch10/index.ts
@@ -1,17 +1,30 @@
-function rating(voyage, history) {
-  return new Rating(voyage, history);
+interface Voyage {
+  zone: string;
+  length: number;
+}
+
+interface Record {
+  zone: string;
+  profit: number;
+}
+
+export default function rating(voyage: Voyage, history: Record[]) {
+  return createRating(voyage, history);
 }
 
 class Rating {
-  constructor(voyage, history) {
+  private voyage: Voyage;
+  private history: Record[];
+
+  constructor(voyage: Voyage, history: Record[]) {
     this.voyage = voyage;
     this.history = history;
   }
 
   get value() {
-    const vpf = voyageProfitFactor(voyage, history);
-    const vr = voyageRisk(voyage);
-    const chr = captainHistoryRisk(voyage, history);
+    const vpf = this.voyageProfitFactor;
+    const vr = this.voyageRisk;
+    const chr = this.captainHistoryRisk;
     if (vpf * 3 > vr + chr * 2) return 'A';
     else return 'B';
   }
@@ -20,7 +33,7 @@ class Rating {
     let result = 1;
     if (this.voyage.length > 4) result += 2;
     if (this.voyage.length > 8) result += this.voyage.length - 8;
-    if (['중국', '동인도'].includes(this.voyage.zone)) result += 4;
+    if ((['중국', '동인도'] as string[]).includes(this.voyage.zone)) result += 4;
     return Math.max(result, 0);
   }
 
@@ -28,7 +41,7 @@ class Rating {
     let result = 1;
     if (this.history.length < 5) result += 4;
     result += this.history.filter(v => v.profit < 0).length;
-    if (this.voyage.zone === '중국' && hasChina(this.history)) result -= 2;
+    if (this.voyage.zone === '중국' && this.hasChinaHistory) result -= 2;
     return Math.max(result, 0);
   }
 
@@ -40,7 +53,7 @@ class Rating {
     let result = 2;
     if (this.voyage.zone === '중국') result += 1;
     if (this.voyage.zone === '동인도') result += 1;
-    if (this.voyage.zone === '중국' && this.hasChinaHistory(this.history)) {
+    if (this.voyage.zone === '중국' && this.hasChinaHistory) {
       result += 3;
 
       if (this.history.length > 10) result += 1;
@@ -54,17 +67,10 @@ class Rating {
   }
 }
 
-function main() {
-  const voyage = { zone: '서인도', length: 10 };
-  const history = [
-    { zone: '동인도', profit: 5 },
-    { zone: '서인도', profit: 15 },
-    { zone: '중국', profit: -2 },
-    { zone: '서아프리카', profit: 7 },
-  ];
+class ExperiencedChinaRating extends Rating {}
 
-  const myRating = rating(voyage, history);
-  console.log(myRating);
+function createRating(voyage: Voyage, history: Record[]) {
+  if (voyage.zone === '중국' && history.some(v => '중국' === v.zone))
+    return new ExperiencedChinaRating(voyage, history);
+  else return new Rating(voyage, history);
 }
-
-main();
